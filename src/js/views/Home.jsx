b948e9dca8647ba9f5886e7b7bec736b5eb50eb5
mendoza099 <sergiomendozadominguez@gmail.com>
2023-12-22 10:31:30 +0000

    landing page

---
+++ src/js/views/Home.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+
+import "../../styles/home.css";
+
+export const Home = () => (
+    <>
+
+
+
+
+        <div className="logo">
+            <img className="imgagen" src="https://res.cloudinary.com/dwkb2dk5r/image/upload/v1703185722/Logo_2_ilf48n.png" alt="" />
+            <p className="titulo">Criptosasun</p>
+
+
+        </div>
+
+
+        <div className="texto">
+
+            <h1 className="texto1"><strong>Estamos construyendo algo grande</strong></h1>
+            <p className="texto2">El futuro comienza aqui </p>
+        </div>
+
+
+        <div className="geometrico">
+
+            <img className="lugar1" src="https://res.cloudinary.com/dwkb2dk5r/image/upload/v1703240666/Sin_t%C3%ADtulo-2_hyv8az.png" />
+        </div>
+
+    </>
+);
+
+
+export default Home;
